867d6fa594619318d8f7434b54632d80da5f00df
Jamil Umar <jamil_umar2000@yahoo.com>
2019-12-22 08:03:24 +0100

    created project with create react app and added files for home page

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import HomePage from './pages/homepage/homepage.component';
+
+
+
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+
+export default App;
